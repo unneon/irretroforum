f9f447a21de383bf4fd75ec030423871ff134e42
Mateusz Cegiełka <mateusz@cegla.net>
2022-11-26 16:04:47 +0100

    Add logging in, registering, and posting in threads

--- database/up.sql
+++ database/up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-    username VARCHAR(256) NOT NULL
+    username VARCHAR(256) UNIQUE NOT NULL,
+    password_phc VARCHAR(128) NOT NULL
 );
 
 CREATE TABLE forums (
@@ -19,4 +20,10 @@ CREATE TABLE posts (
     thread UUID REFERENCES threads(id) NOT NULL,
     author UUID REFERENCES users(id) NOT NULL,
     content VARCHAR(8192) NOT NULL
+);
+
+CREATE TABLE sessions (
+    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    "user" UUID REFERENCES users(id) NOT NULL,
+    token CHAR(32) NOT NULL
 );
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,44 @@ mod config;
 mod view;
 
 use crate::view::{begin_html, end_html};
-use axum::extract::{ConnectInfo, Path};
+use argon2::password_hash::SaltString;
+use argon2::{PasswordHasher, PasswordVerifier};
+use axum::extract::{ConnectInfo, Path, State};
+use axum::headers::Cookie;
+use axum::http::header::{HeaderName, SET_COOKIE};
 use axum::http::{Request, StatusCode};
 use axum::middleware::Next;
-use axum::response::{Html, Response};
-use axum::{extract::State, routing::get, RequestPartsExt, Router};
+use axum::response::{AppendHeaders, Html, Redirect, Response};
+use axum::routing::{get, post};
+use axum::{Form, RequestPartsExt, Router, TypedHeader};
+use rand::Rng;
+use serde::Deserialize;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio_postgres::NoTls;
-use tracing::{event, info, span, Instrument, Level};
+use tracing::{debug, event, info, span, Instrument, Level};
 use uuid::Uuid;
 
 #[derive(Clone)]
 struct App {
     database: Arc<tokio_postgres::Client>,
+}
+
+#[derive(Debug, Deserialize)]
+struct PostInThreadForm {
+    content: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct LogInForm {
+    username: String,
+    password: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct RegisterForm {
+    username: String,
+    password: String,
 }
 
 async fn show_homepage(app: State<App>) -> Html<String> {
@@ -89,7 +113,41 @@ async fn show_thread(Path(thread_id): Path<Uuid>, app: State<App>) -> Html<Strin
         html +=
             &format!("<a href=\"/user/{user_id}\">{user_username}</a>:<p>{safe_post_content}</p>");
     }
+    html += &format!(
+        include_str!("html/thread-post-form.html"),
+        thread_id = thread_id
+    );
     end_html(html)
+}
+
+async fn post_in_thread(
+    Path(thread_id): Path<Uuid>,
+    cookies: TypedHeader<Cookie>,
+    app: State<App>,
+    form: Form<PostInThreadForm>,
+) -> Redirect {
+    let session_hex = cookies.get("session").unwrap();
+    debug!(
+        session = session_hex,
+        "Checking authorization for session token"
+    );
+    let user = app
+        .database
+        .query_one(
+            "SELECT \"user\" FROM sessions WHERE token = $1",
+            &[&session_hex],
+        )
+        .await
+        .unwrap();
+    let user_id: Uuid = user.get(0);
+    app.database
+        .execute(
+            "INSERT INTO posts (thread, author, content) VALUES ($1, $2, $3)",
+            &[&thread_id, &user_id, &form.content],
+        )
+        .await
+        .unwrap();
+    Redirect::to(&format!("/thread/{thread_id}"))
 }
 
 async fn show_user(Path(user_id): Path<Uuid>, app: State<App>) -> Html<String> {
@@ -102,6 +160,75 @@ async fn show_user(Path(user_id): Path<Uuid>, app: State<App>) -> Html<String> {
     let mut html = begin_html(&format!("{user_username}'s profile"));
     html += &format!("<p>{user_username}</p>");
     end_html(html)
+}
+
+async fn show_login_form() -> Html<String> {
+    let mut html = begin_html("Log in to Irretroforum");
+    html += include_str!("html/login-form.html");
+    end_html(html)
+}
+
+async fn login(
+    app: State<App>,
+    form: Form<LogInForm>,
+) -> (AppendHeaders<HeaderName, String, 1>, Redirect) {
+    let user = app
+        .database
+        .query_one(
+            "SELECT id, password_phc FROM users WHERE username = $1",
+            &[&form.username],
+        )
+        .await
+        .unwrap();
+    let user_id: Uuid = user.get(0);
+    let password_phc: &str = user.get(1);
+    let password_hash = argon2::PasswordHash::new(password_phc).unwrap();
+    argon2::Argon2::default()
+        .verify_password(form.password.as_bytes(), &password_hash)
+        .unwrap();
+    let session_token: [u8; 128 / 8] = rand::thread_rng().gen();
+    let session_token_hex = hex::encode(&session_token);
+    app.database
+        .execute(
+            "INSERT INTO sessions (\"user\", token) VALUES ($1, $2)",
+            &[&user_id, &session_token_hex],
+        )
+        .await
+        .unwrap();
+    (
+        AppendHeaders([(
+            SET_COOKIE,
+            format!("session={session_token_hex}; Secure; HttpOnly; SameSite=Strict"),
+        )]),
+        Redirect::to("/"),
+    )
+}
+
+async fn show_register_form() -> Html<String> {
+    let mut html = begin_html("Register on Irretroforum");
+    html += include_str!("html/register-form.html");
+    end_html(html)
+}
+
+async fn register(app: State<App>, form: Form<RegisterForm>) -> Redirect {
+    let password_kdf = argon2::Argon2::new(
+        argon2::Algorithm::Argon2id,
+        argon2::Version::V0x13,
+        argon2::Params::new(37 * 1024, 1, 1, None).unwrap(),
+    );
+    let salt = SaltString::generate(&mut rand::thread_rng());
+    let password_phc = password_kdf
+        .hash_password(form.password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
+    app.database
+        .execute(
+            "INSERT INTO users (username, password_phc) VALUES ($1, $2)",
+            &[&form.username, &password_phc],
+        )
+        .await
+        .unwrap();
+    Redirect::to("/")
 }
 
 async fn show_css() -> &'static str {
@@ -147,7 +274,10 @@ async fn main() {
         .route("/", get(show_homepage))
         .route("/forum/:id", get(show_forum))
         .route("/thread/:id", get(show_thread))
+        .route("/thread/:id/post", post(post_in_thread))
         .route("/user/:id", get(show_user))
+        .route("/login", get(show_login_form).post(login))
+        .route("/register", get(show_register_form).post(register))
         .route("/style.css", get(show_css))
         .layer(axum::middleware::from_fn(logging_middleware));
     tokio::spawn(database.1);
